d17d02da4bd0ef3358cc38b4e4893aa7811ae620
Ziyad Meribaa <zmeribaa@e2r1p6.1337.ma>
2022-09-20 23:56:47 +0100

    Ill complete the multiplexing tomorrow

--- Server.cpp
+++ Server.cpp
@@ -1,5 +1,6 @@
 #include "Server.hpp"
 #include "Request.hpp"
+#include "Socket."
 
 Server::Server()
 {
@@ -44,7 +45,13 @@ void Server::lessgo()
     int server_fd, new_socket; long valread;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
+	struct fd_set  backup_fds;
+	struct fd_set	write_fds;
+	struct fd_set	read_fds;
+
+
 	int opt = 1;
+	int max_fds;
 	std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 54623\n\n Hello";
 	
     
@@ -54,7 +61,8 @@ void Server::lessgo()
         perror("In socket");
         exit(EXIT_FAILURE);
     }
-    
+    max_fds = server_fd;
+	fcntl(server_fd, F_SETFL, O_NONBLOCK);
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = inet_addr(keys["server_name"].c_str());
@@ -83,15 +91,40 @@ void Server::lessgo()
         perror("In listen");
         exit(EXIT_FAILURE);
     }
+
+	FD_ZERO(&backup_fds);
+	FD_ZERO(&write_fds);
+	FD_ZERO(&read_fds);
+
+// map<int, Request> request_mapper; (client_fd, req);
+
     while(1)
     {
+//		read_fds = write_fds = backup_fds;
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-		
+
+
+//			select call; select(max_fds, read_fds, write_fds, NULL, NULL);
+
+//          for i in max_fds:				
+	//				i inclus read_fds: (FD_ISSET(i, read_fds))
+	//					if i == server_fd:
+							// accept new connection
+							//
+						//else
+						//      recv(i, buff)
+						//      if (map.find())
+	//				i inclus write_fds:
+	//					i is a client socket => write response
+//				
+
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
         {
             perror("In accept");
             exit(EXIT_FAILURE);
         }
+		// set socket as non blocking
+		fcntl(new_socket, F_SETFL, O_NONBLOCK);
         
         char buffer[30000] = {0};
         valread = recv( new_socket , buffer, 30000,0);
--- Socket.hpp
+++ Socket.hpp
@@ -6,7 +6,7 @@
 /*   By: zmeribaa <zmeribaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 15:37:33 by zmeribaa          #+#    #+#             */
-/*   Updated: 2022/09/16 21:17:40 by zmeribaa         ###   ########.fr       */
+/*   Updated: 2022/09/20 01:44:29 by zmeribaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,12 @@ class Socket {
 public:
 	Socket(void);
 	~Socket(void);
+	class SocketException : public std::exception{
+		private:
+			std::string _err;
+		public:
+			SocketException(std::string const &) throw() : _err(err){}
+			virtual ~SocketException() throw(){}
+			virtual const char *what() const throw(){ return _err;}
+	};
 };
